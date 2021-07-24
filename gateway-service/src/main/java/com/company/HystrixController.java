256b9aa3611135f4002da732303adddea045d46a
Mahammad Eminov <eminov-2001@inbox.ru>
2021-07-24 23:56:21 +0500

    .

---
+++ gateway-service/src/main/java/com/company/HystrixController.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fb")
+public class HystrixController {
+
+    @GetMapping("/users")
+    public String accountFallback(){
+        return "Customer Service is not available.";
+    }
+
+    @GetMapping("/products")
+    public String ticketFallback(){
+        return "Product Service is not available.";
+    }
+}
